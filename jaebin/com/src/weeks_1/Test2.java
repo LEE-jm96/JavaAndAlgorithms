c3fcc308d4ce52f33de4d3cc7d5c7edb6c5093dd
jaebin1234 <jaebin2586@gmail.com>
2022-01-24 21:51:15 +0900

    feat : 테스트 입니다.

---
+++ jaebin/com/src/weeks_1/Test2.java
@@ -0,0 +1,13 @@
+package weeks_1;
+
+public class Test2 {
+    private static int a;
+    private static int b;
+
+    public static void main(String[] args) {
+        a = 12345;
+        b = 12345;
+        System.out.println("두번째 테스트");
+        System.out.println(a+b);
+    }
+}
