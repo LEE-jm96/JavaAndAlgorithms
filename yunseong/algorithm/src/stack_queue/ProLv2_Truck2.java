33bb9b184aa6c9db4773110fb91ab58379610558
YunSeongOh <gagizzang92@gmail.com>
2022-02-06 22:25:38 +0900

    feat : 2/4, 프로그래머스 레벨2 [다리를 지나는트럭 문제] - 반복문 하나사용

---
+++ yunseong/algorithm/src/stack_queue/ProLv2_Truck2.java
@@ -0,0 +1,48 @@
+package stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProLv2_Truck2 {
+
+	public static void main(String[] args) {
+		
+		int bridge_length = 100;
+		int weight = 100;
+		int[] truck_weights = {10};
+		System.out.println(solution(bridge_length, weight, truck_weights));
+	}
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+		int answer = 0;
+		int sum = 0;
+		
+		Queue<Integer> bridge = new LinkedList<Integer>();
+	
+		for(int i = 0 ; i < truck_weights.length ;) {
+			if(bridge.size() < bridge_length) {
+				if(weight >= sum + truck_weights[i]) {
+					bridge.add(truck_weights[i]);
+					sum += truck_weights[i];
+					i++;
+				}else {
+					bridge.add(0); 
+				}
+				answer++;
+			}else if (bridge.size() == bridge_length) {
+				int truck = bridge.poll(); 	
+				sum -= truck;
+	
+				if(weight >= sum + truck_weights[i]) {
+					bridge.add(truck_weights[i]);
+					sum += truck_weights[i];
+					i++;
+				}else {
+					bridge.add(0);
+				}
+				answer++;
+			}
+		}
+		
+		return answer+bridge_length;
+    }
+}
