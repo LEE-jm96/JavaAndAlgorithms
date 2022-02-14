faeeb5ecdb311ed618987b2c2f4eac4c339fc674
he2so0 <heesoo9913@gmail.com>
2022-02-14 21:20:31 +0900

    feat : 2/14, 백준 알고리즘 : [2751번 수 정렬하기 2 문제]

---
+++ heesoo/4_weeks_sort/baekjoon2751.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class baekjoon2751 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[num];
+		
+		StringBuffer result = new StringBuffer();
+		
+		for(int i=0;i<num;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int item : arr) {
+			result.append(item+"\n");
+		}
+		System.out.println(result);
+	}
+}
