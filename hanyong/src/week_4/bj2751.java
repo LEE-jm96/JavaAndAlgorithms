968c3733277f99832e8f6213c213690e12f3caef
Quence1151 <dlgksdyd1151@naver.com>
2022-02-14 18:15:07 +0900

    feat : 2/14, 백준 알고리즘 : [2751번 수 정렬하기 2 문제]

---
+++ hanyong/src/week_4/bj2751.java
@@ -0,0 +1,27 @@
+package week_4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class bj2751 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder answer = new StringBuilder();
+        int N = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            list.add(sc.nextInt());
+        }
+        Collections.sort(list);
+        for(Integer c : list) {
+            answer.append(c+"\n");
+        }
+        System.out.println(answer);
+        sc.close();
+    }
+
+}
