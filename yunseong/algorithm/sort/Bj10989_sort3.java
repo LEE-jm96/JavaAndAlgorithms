cc4ccb043b6ea7d4b05c2970210567dffee7fcb7
YunSeongOh <gagizzang92@gmail.com>
2022-02-16 00:28:09 +0900

    feat : 2/15, 백준 알고리즘 : [10989 수 정렬하기 3번 문제]

--- yunseong/algorithm/sort/Bj10989_sort3.java
+++ yunseong/algorithm/sort/Bj10989_sort3.java
@@ -5,39 +5,78 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Bj10989_sort3 {
+
+
+
+
+
+// 힙정렬 시간초과..
+//    //배열요소 a[idx1]과 a[idx2]의 값을 바꿉니다.
+//    static void swap(int[] a, int idx1, int idx2) {
+//        int t = a[idx1];
+//        a[idx1] = a[idx2];
+//        a[idx2] = t;
+//    }
+//    //a[left] ~ a[right] 를 힙으로 만듭니다.
+//    static void downHeap(int[] a, int left, int right) {
+//        int temp = a[left];//루트
+//        int child;//큰 값을 가진 노드
+//        int parent;//부모
+//
+//        for(parent = left;parent<(right+1) /2 ; parent=child){
+//            int cl = parent* 2 +1; //왼쪽자식
+//            int cr = cl+1;//오른쪽 자식
+//            child = (cr<=right && a[cr] >a[cl])? cr:cl;//큰 값을 가진 노드를 자식에 대입
+//            if(temp>= a[child])
+//                break;
+//            a[parent] = a[child];
+//        }
+//        a[parent] = temp;
+//    }
+//
+//    //힙정렬
+//    static void heapSort(int[] a, int n) {
+//        for(int i = (n-1)/2;i>=0; i--) {
+//            downHeap(a, i ,n-1);
+//        }
+//        for(int i = n -1; i>0 ;i--) {
+//            swap(a,0,i);
+//            downHeap(a,0,i-1);
+//        }
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i ++) {
+//            arr[i] = Integer.parseInt(br.readLine());
+//        }
+//        heapSort(arr, n);
+//
+//        for(int i = 0; i< n ; i++) {
+//            System.out.println(arr[i]);
+//        }
+//
+//
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] cnt = new int[10001];
 
+        for(int i = 0 ; i < n ; i++) {
+            cnt[Integer.parseInt(br.readLine())]++;
+        }
+        br.close();
 
-
-
-
-
-
-//      카운트 정렬... 실패!
-//
-//        boolean[] arr = new boolean[10001];
-//        int[] cnt = new int[10001];
-//
-//
-//        for(int i = 0 ; i < cnt.length ; i++) {
-//            cnt[i] = 1;
-//        }
-//
-//        for(int i = 0 ; i < n ; i++ ){
-//            int num = Integer.parseInt(br.readLine());
-//            if(arr[num]){
-//                cnt[num]++;
-//            }
-//            arr[num] = true;
-//        }
-//        for(int i = 0 ; i < arr.length ; i++) {
-//            if(arr[i]){
-//                for(int j = 0; j < cnt[i] ; j++){
-//                    System.out.println(i);
-//                }
-//            }
-//        }
+        for(int i = 0 ; i < cnt.length ; i++) {
+            while(cnt[i]>0){ //i = 1 // val = 2
+                System.out.println(i);
+                cnt[i]--;
+            }
+        }
     }
 }
