61d8ef4b8bb1974d449f67b15a4902e9536637f1
he2so0 <heesoo9913@gmail.com>
2022-02-27 10:05:34 +0900

    feat : 2/25, 백준 알고리즘 : [18310번 안테나 문제]

---
+++ heesoo/5_weeks_sort/baekjoon18310.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon18310 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			if(st.hasMoreTokens())
+				arr[i]= Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		if(n%2 ==0) {
+			System.out.println(arr[n/2-1]);
+		}else {
+			System.out.println(arr[n/2]);
+		}
+	}
+}
