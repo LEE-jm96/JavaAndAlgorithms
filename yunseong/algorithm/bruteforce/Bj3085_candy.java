d3dbcd44fc710f4c1136403bae181c5723d6dda0
YunSeongOh <gagizzang92@gmail.com>
2022-03-10 08:34:28 +0900

    fix : 3/7 백준 알고리즘 [3085번 사탕게임 문제]

--- yunseong/algorithm/bruteforce/Bj3085_candy.java
+++ yunseong/algorithm/bruteforce/Bj3085_candy.java
@@ -1,70 +1,76 @@
 package algorithm.bruteforce;
 
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Bj3085_candy {
-    static int check(char[][] a) {
-        int n = a.length;
-        int ans = 1;
-        for (int i = 0; i < n; i++) {
-            int cnt = 1;
-            for (int j = 1; j < n; j++) {
-                if (a[i][j] == a[i][j - 1]) {
-                    cnt += 1;
-                } else {
-                    cnt = 1;
-                }
-                if (ans < cnt)
-                    ans = cnt;
-            }
-            cnt = 1;
-            for (int j = 1; j < n; j++) {
-                if (a[j][i] == a[j - 1][i]) {
-                    cnt += 1;
-                } else {
-                    cnt = 1;
-                }
-                if (ans < cnt)
-                    ans = cnt;
-            }
-        }
-        return ans;
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        char[][] a = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.next().toCharArray();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        char[][] arr = new char[n][n];
+        for (int i = 0; i < n ; i++) {
+            String str = s.next();
+            for (int j = 0; j < n; j++) arr[i][j] = str.charAt(j);
         }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j + 1 < n) {
-                    char t = a[i][j];
-                    a[i][j] = a[i][j + 1];
-                    a[i][j + 1] = t;
-                    int temp = check(a);
-                    if (ans < temp)
-                        ans = temp;
-                    t = a[i][j];
-                    a[i][j] = a[i][j + 1];
-                    a[i][j + 1] = t;
-                }
-                if (i + 1 < n) {
-                    char t = a[i][j];
-                    a[i][j] = a[i + 1][j];
-                    a[i + 1][j] = t;
-                    int temp = check(a);
-                    if (ans < temp)
-                        ans = temp;
-                    t = a[i][j];
-                    a[i][j] = a[i + 1][j];
-                    a[i + 1][j] = t;
+        List<Integer> list = switching(arr);
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list.get(0));
+
+    }
+    public static List<Integer> switching(char[][] arr){
+        List<Integer> list = new ArrayList<>();
+        char[][] origin = arr;
+
+        for(int i = 0 ; i < arr.length ;i++) {
+            for(int j = 0 ; j < arr.length-1 ; j++) {
+                if (arr[i][j] != arr[i][j + 1]) {
+                    char temp = arr[i][j];
+                    arr[i][j] = arr[i][j + 1];
+                    arr[i][j + 1] = temp;
+                    list.add(checking(arr));
+                    arr = origin;
                 }
             }
         }
-        System.out.println(ans);
+        for(int i = 0 ; i < arr.length ;i++) {
+            for(int j = 0 ; j < arr.length-1 ; j++) {
+                if (arr[j][i] != arr[j+1][i]) {
+                    char temp = arr[j][i];
+                    arr[j][i] = arr[j+1][i];
+                    arr[j+1][i] = temp;
+                    list.add(checking(arr));
+                    arr = origin;
+                }
+            }
+        }
+        return list;
+
+    }
+    public static int checking(char[][] arr) {
+        int answer = 1;
+        int sum = 1;
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[i][j] == arr[i][j + 1]) {
+                    sum++;
+                }
+            }
+            if(answer < sum){
+                answer = sum;
+            }
+            sum = 1;
+        }
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j][i] == arr[j+1][i]) {
+                    sum++;
+                }
+            }
+            if(answer < sum){
+                answer = sum;
+            }
+            sum = 1;
+        }
+        return answer;
     }
 }
