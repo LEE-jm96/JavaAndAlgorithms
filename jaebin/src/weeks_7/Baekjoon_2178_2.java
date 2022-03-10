6a10c8bf09d2f65181687efd84c7a1125a0cdd21
jaebin1234 <jaebin2586@gmail.com>
2022-03-11 01:38:33 +0900

    feat : 3/10, 백준 알고리즘 : [2178번 미로탐색 문제]

--- jaebin/src/weeks_7/Baekjoon_2178_2.java
+++ jaebin/src/weeks_7/Baekjoon_2178_2.java
@@ -8,60 +8,74 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 //백준 2178번 : 미로탐색, 브루트 포스
-public class Baekjoon_2178 {
+public class Baekjoon_2178_2 {
+    /**
+     * 미로의 각 칸이 탐색 대상 노드이다.
+     * 멤버변수는 좌표(행,열), 지금까지 이동한 거리
+     * 방문한 칸을 다시 방문하지 않기 위한 visited 배열
+     */
+    public static class Node {
+        int row;
+        int col;
+        int distance;
+
+        public Node(int row, int col, int distance) {
+            this.row = row;
+            this.col = col;
+            this.distance = distance;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int arr[][] = new int[N][M];
+        char arr[][] = new char[N][M];
         boolean visited[][] = new boolean[N][M];
         // 좌표를 저장 할 큐
-        Queue<int[]> nextQueue = new LinkedList<>();
+        Queue<Node> nextQueue = new LinkedList<>();
 
         for (int i = 0; i < arr.length; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = Character.getNumericValue(s.charAt(j));
-            }
+            arr[i] = br.readLine().toCharArray();
         }
-        bfs(arr, visited, nextQueue);
-        System.out.println(arr[N - 1][M - 1]);
+        int result = bfs(arr, visited, nextQueue);
+        System.out.println(result);
     }
 
     public static boolean rangeCheck(int x, int y, int N, int M) {
         return x >= 0 && x < N && y >= 0 && y < M;
     }
 
-    public static int[][] bfs(int arr[][], boolean visited[][], Queue<int[]> nextQueue) {
-        //상하
-        int dx[] = {-1, 1, 0, 0};
-        //좌우
-        int dy[] = {0, 0, -1, 1};
-
+    public static int bfs(char arr[][], boolean visited[][], Queue<Node> nextQueue) {
         int N = arr.length;
         int M = arr[0].length;
-        nextQueue.add(new int[]{0, 0});
-        while (!nextQueue.isEmpty()) {
-            int now[] = nextQueue.poll();
-            int nowX = now[0];
-            int nowY = now[1];
 
-            //상하좌우 
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-                //범위를 벗어나거나, 방문한적이 있거나, 방문할 노드가 0이면 
-                if ((!rangeCheck(nextX, nextY, N, M))
-                        || visited[nextX][nextY]
-                        || arr[nextX][nextY] == 0) continue;
-                nextQueue.add(new int[]{nextX, nextY});
-                // 지난가면서 누적합 계산 (최종 (N-1,M-1)의 최소 누적합을 알아야하기 때문이다)
-                arr[nextX][nextY] = arr[nowX][nowY] + 1;
-                visited[nextX][nextY] = true;
-                // 다시 while문을 돌면서 검사 
-            }
+        nextQueue.add(new Node(0, 0, 1));
+        while (!nextQueue.isEmpty()) {
+            //노드를 삭제하면서 꺼냄
+            Node currentNode = nextQueue.remove();
+            int nowX = currentNode.row;
+            int nowY = currentNode.col;
+
+            //범위를 벗어났다면
+            if ((!rangeCheck(nowX, nowY, N, M))) continue;
+            // 목적지에 도착했으면 리턴
+            if (nowX == N - 1 && nowY == M - 1) return currentNode.distance;
+            // 방문한적이 있거나, 방문할 노드가 0이면
+            if (visited[nowX][nowY]
+                    || arr[nowX][nowY] == '0') continue;
+            //현재 노드 방문 완료
+            visited[nowX][nowY] = true;
+            // 상하좌우 인접한 노드들을 전부 큐에 넣음
+            nextQueue.add(new Node(nowX - 1, nowY, currentNode.distance + 1));
+            nextQueue.add(new Node(nowX + 1, nowY, currentNode.distance + 1));
+            nextQueue.add(new Node(nowX, nowY - 1, currentNode.distance + 1));
+            nextQueue.add(new Node(nowX, nowY + 1, currentNode.distance + 1));
+            // 다시 while문을 돌면서 검사 
+
         }
-        return arr;
+        // 탐색 실패
+        return -1;
     }
 }
