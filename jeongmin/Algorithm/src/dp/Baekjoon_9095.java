f1236868f342f716874b8a29f77f24cdcefe8581
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-04-06 17:18:52 +0900

    feat : 4/5 백준알고리즘[9095번 1, 2, 3 더하기문제]

---
+++ jeongmin/Algorithm/src/dp/Baekjoon_9095.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Baekjoon_9095 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for(int i = 4; i <= 10; i++) {
+			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+		}
+		
+		for(int j = 0; j < T; j++) {
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(dp[n]);
+		}
+	}
+}
