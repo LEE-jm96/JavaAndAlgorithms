b0213d9bed0a8d2a5bd8edbf8ba69908cf26e5e1
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-04-10 21:20:05 +0900

    feat : 4/8 백준알고리즘[1904번 01타일문제]

---
+++ jeongmin/Algorithm/src/dp/Baekjoon_1904.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.*;
+
+public class Baekjoon_1904 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];	
+		dp[0] = 0;
+    
+		for(int i = 1; i < dp.length; i++) {
+			if(i == 1) dp[i] = 1;
+			else if(i == 2) dp[i] = 2;
+			else dp[i] = (dp[i - 2] + dp[i - 1]) % 15476;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
